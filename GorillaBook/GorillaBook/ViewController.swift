6bdb36dd3df9a451abc9bbb0e1ec641e0865145c
Christian Collazos <christian.collazos@bizagi.com>
2019-11-11 22:31:38 -0500

    Initial configuration and splashScreen

---
+++ GorillaBook/GorillaBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorillaBook
+//
+//  Created by Christian Collazos on 11/11/19.
+//  Copyright © 2019 Christian Collazos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
